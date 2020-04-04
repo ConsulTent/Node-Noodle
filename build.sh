a887ad5c30d3856af9086561c7536363c35958b6
azilber <azilber@consultent.ltd>
2020-04-04 16:30:35 +0800

    workflow added, and build script updated

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 export GOPATH=$HOME/go
 go test
 if [ $? == 0 ]; then
+  if [ "${GOOS}" == "windows" ]; then
+go build -i -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle.exe *.go
+echo 'noodle.exe'
+  else
 go build -i -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle *.go
+echo 'noodle'
+  fi
 fi
