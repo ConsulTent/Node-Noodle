233ae775fe581094d4c99eaad54c7efd754bcb86
Alexey Zilber <alexey.zilber@qigroup.com>
2021-04-21 13:12:10 +0800

    Detect github actions

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 export GOPATH=$HOME/go
+if [ "$GITHUB_ACTIONS" == "true" ];then 
+go test 1> debug.out
+else
 go test
+fi
+
 if [ $? == 0 ]; then
   if [ "${GOOS}" == "windows" ]; then
-go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle.exe *.go
+    if [ "$GITHUB_ACTIONS" == "true" ];then 
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle.exe *.go 1>> debug.out
 echo 'noodle.exe'
+    else
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle.exe *.go    
+    fi
   else
-go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle *.go
+    if [ "$GITHUB_ACTIONS" == "true" ];then 
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle *.go 1>> debug.out
 echo 'noodle'
+    else
+go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle *.go
+    fi
   fi
 fi
