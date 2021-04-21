0694cbbdb9e61a59f90bb102919e16fea5b01d27
Alexey Zilber <alexey.zilber@qigroup.com>
2021-04-21 14:11:23 +0800

    build.sh cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GOPATH=$HOME/go
-if [ "$GITHUB_ACTIONS" == "true" ];then 
+if [ "${GITHUB_ACTIONS}" == "true" ]; then
 go test 1> debug.out
 else
 go test
@@ -8,14 +8,14 @@ fi
 
 if [ $? == 0 ]; then
   if [ "${GOOS}" == "windows" ]; then
-    if [ "$GITHUB_ACTIONS" == "true" ];then 
+    if [ "${GITHUB_ACTIONS}" == "true" ]; then 
 go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle.exe *.go 1>> debug.out
 echo 'noodle.exe'
     else
 go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle.exe *.go    
     fi
   else
-    if [ "$GITHUB_ACTIONS" == "true" ];then 
+    if [ "${GITHUB_ACTIONS}" == "true" ]; then 
 go build -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o noodle *.go 1>> debug.out
 echo 'noodle'
     else
